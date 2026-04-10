e8dde0f6d989df589d360632754f2f76e4812914
AvidAardvark <144076718+AvidAardvark@users.noreply.github.com>
2026-04-09 23:16:57 -0700

    Create vm.c

---
+++ Chapter 19 Challenge 2/vm.c
@@ -0,0 +1,13 @@
+static void concatenate() {
+  ObjString* b = AS_STRING(pop());
+  ObjString* a = AS_STRING(pop());
+
+  int length = a->length + b->length;
+  char* chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString* result = makeString(true, chars, length); 
+  push(OBJ_VAL(result));
+}
